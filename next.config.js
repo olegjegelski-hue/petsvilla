6011399cf26ef99692b5dfe1d00db7be5eb796b6
olegjegelski-hue <oleg.jegelski@gmail.com>
2025-11-17 23:23:18 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,27 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: false,
   },
-  images: { unoptimized: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.abacus.ai',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.abacusaicdn.net',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
