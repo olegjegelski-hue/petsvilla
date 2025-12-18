c913ca1af459dad8bd673a1cdb5d82ab45dbd84d
olegjegelski-hue <oleg.jegelski@gmail.com>
2025-12-18 21:45:12 +0200

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
