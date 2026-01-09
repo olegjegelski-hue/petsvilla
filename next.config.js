81c0f7544d62acca36f1743fcd7fa1f2d8ea3513
olegjegelski-hue <oleg.jegelski@gmail.com>
2026-01-10 00:03:25 +0200

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,15 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  images: { unoptimized: true },
+distDir: process.env.NEXT_DIST_DIR || '.next',
+eslint: {
+ignoreDuringBuilds: true,
+},
+typescript: {
+ignoreBuildErrors: false,
+},
+images: { unoptimized: true },
 };
+> 
 
-module.exports = nextConfig;
+> module.exports = nextConfig;
+>
